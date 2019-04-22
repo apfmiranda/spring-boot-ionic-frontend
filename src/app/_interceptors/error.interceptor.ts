cfc054cda725a5726d64fb9099d8c8a7a65875c4
Alexandre Pires Ferreira de Miranda <alexandre.miranda@gmail.com>
2019-04-22 13:48:41 -0300

    Tratamento padrão para outros erros com Alert

--- src/app/_interceptors/error.interceptor.ts
+++ src/app/_interceptors/error.interceptor.ts
@@ -3,40 +3,70 @@ import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/c
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { StorageService } from '../_services/storage.service';
+import { AlertController } from '@ionic/angular';
 
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-    constructor(public storage: StorageService){}
+    constructor(
+        public storage: StorageService,
+        public alertController: AlertController
+        ) {}
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
         return next.handle(request)
             .pipe(
                 catchError(err => {
-                    let errorObj = err;
-                    if (errorObj.error) {
-                        errorObj = errorObj.console.error;
-                        if (!errorObj.status) {
-                            errorObj = JSON.parse(errorObj);
-                        }
-                    }
 
-                    switch (errorObj.status) {
+                    switch (err.status) {
+                        case 401:
+                            this.handled401();
+                            console.log('case 401:');
+                            break;
                         case 403:
-                            this.hadle403();
+                            this.handled403();
                             break;
                         default:
+                            this.handledDefaultError(err);
                             break;
                     }
+
+                    // let errorObj = err;
+                    // if (errorObj.error) {
+                    //     errorObj = errorObj.console.error;
+                    //     if (!errorObj.status) {
+                    //         errorObj = JSON.parse(errorObj);
+                    //     }
+                    // }
                     const error = err.error.message || err.statusText;
                     return throwError(error);
                 })
             );
     }
 
-    hadle403() {
+    async handledDefaultError(err: any) {
+        const alert = await this.alertController.create({
+            header: 'ERRO ' + err.status,
+            subHeader: err.error,
+            message: err.message,
+            buttons: ['OK']
+          });
+          await alert.present();
+    }
+
+    async handled401() {
+        const alert = await this.alertController.create({
+            header: 'ERRO 401',
+            subHeader: 'Falha de autenticação',
+            message: 'Email ou senha incorretos',
+            buttons: ['OK']
+          });
+          await alert.present();
+    }
+
+    handled403() {
         this.storage.setLocalUser(null);
     }
 }
